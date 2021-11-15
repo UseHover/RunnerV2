889d998859a8661b16e353029a7fac243fd304c1
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2021-11-15 23:24:15 +0100

    navigate settings bug fix

--- app/src/main/java/com/hover/runner/home/AbstractNavigationActivity.kt
+++ app/src/main/java/com/hover/runner/home/AbstractNavigationActivity.kt
@@ -10,11 +10,17 @@ import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hover.runner.R
 import com.hover.runner.actions.navigation.ActionNavigationInterface
+import com.hover.runner.login.activities.LoginActivity
 import com.hover.runner.parser.navigation.ParserNavigationInterface
+import com.hover.runner.settings.navigation.SettingsNavigationInterface
 import com.hover.runner.transactions.navigation.TransactionNavigationInterface
 import com.hover.runner.webview.WebViewActivity
 
-abstract class AbstractNavigationActivity : SDKCallerActivity() , ActionNavigationInterface, TransactionNavigationInterface, ParserNavigationInterface {
+ abstract class AbstractNavigationActivity : SDKCallerActivity() ,
+    ActionNavigationInterface,
+    TransactionNavigationInterface,
+    ParserNavigationInterface,
+    SettingsNavigationInterface{
     private lateinit var navController : NavController
 
     fun setupNavigation() {
@@ -59,6 +65,11 @@ abstract class AbstractNavigationActivity : SDKCallerActivity() , ActionNavigati
 
     override fun navTransactionDetails(uuid: String) {
          TODO("Not yet implemented")
+     }
+
+     override fun navLoginAndFinish() {
+         startActivity(Intent(this, LoginActivity::class.java))
+         finishAffinity()
      }
 
 
