4ae5b161e894d6b2b48e396d418fbb2fa213be16
kutalek <davkutalek@gmail.com>
2022-04-21 14:24:56 -0700

    Add a pie graph to transaction list
    Use com.github.blackfizz:eazegraph to summarize the statuses.
    
    Closes #15

--- app/src/main/java/com/hover/runner/transactions/TransactionsViewModel.kt
+++ app/src/main/java/com/hover/runner/transactions/TransactionsViewModel.kt
@@ -1,6 +1,8 @@
 package com.hover.runner.transactions
 
 import androidx.lifecycle.*
+import com.hover.sdk.actions.HoverAction
+import com.hover.sdk.parsers.HoverParser
 import com.hover.sdk.transactions.Transaction
 
 class TransactionsViewModel(repo: TransactionsRepo) : ViewModel() {
